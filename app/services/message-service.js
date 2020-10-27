d4e0a0a96add168449fef9755ed4554c6b85bbda
Paul Andrews <paul.andrews@defra.gov.uk>
2020-10-27 22:10:31 +0000

    Create XLSX on application event

--- app/services/message-service.js
+++ app/services/message-service.js
@@ -1,6 +1,7 @@
 const auth = require('@azure/ms-rest-nodeauth')
 const MessageReceiver = require('./messaging/message-receiver')
 const config = require('../config').messaging
+const XLSX = require('xlsx')
 
 process.on('SIGTERM', async () => {
   await messageService.closeConnections()
@@ -12,23 +13,39 @@ process.on('SIGINT', async () => {
   process.exit(0)
 })
 
+function createSpreadsheet (message) {
+  const wb = XLSX.utils.book_new()
+
+  wb.Props = {
+    Title: 'This is a test',
+    Author: 'FFC EOI'
+  }
+
+  wb.SheetNames.push('First Sheet')
+  const ws = XLSX.utils.json_to_sheet([message])
+  wb.Sheets['First Sheet'] = ws
+
+  XLSX.writeFile(wb, 'test123.xlsx')
+
+  console.log(`Created spreadsheet from application ${message.confirmationId}`)
+}
+
 class MessageService {
   constructor (credentials) {
     this.closeConnections = this.closeConnections.bind(this)
 
     const receiveApplicationAction = async message => {
-      const messageObj = JSON.parse(message)
-
       console.log('\n#####\nEOI submitted message received:')
-      console.log(`Confirmation ID: ${messageObj.payload.confirmationId}\n`)
+      const messageObj = JSON.parse(message)
+      createSpreadsheet(messageObj)
     }
 
     const receiveContactAction = async message => {
       const messageObj = JSON.parse(message)
 
       console.log('\n#####\nSend email message received:')
-      console.log(`Email address: ${messageObj.payload.emailAddress}`)
-      console.log(`Magic link: ${messageObj.payload.magicLink}\n`)
+      console.log(`Email address: ${messageObj.emailAddress}`)
+      console.log(`Magic link: ${messageObj.magicLink}\n`)
     }
 
     this.applicationReceiver = new MessageReceiver('application-topic-receiver', config.applicationTopic, credentials, receiveApplicationAction)
